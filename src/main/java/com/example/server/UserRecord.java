7c5edef7db863106ca02b7919360c64d283863b9
adelinavisan97 <visanadelina@yahoo.com>
2025-03-10 19:08:26 +0000

    minor fixes

--- src/main/java/com/example/server/UserRecord.java
+++ src/main/java/com/example/server/UserRecord.java
@@ -3,7 +3,6 @@ package com.example.server;
 public class UserRecord {
     private String email;
     private String fullName;
-    // constructor, getters...
     public UserRecord(String email, String fullName) {
         this.email = email;
         this.fullName = fullName;
